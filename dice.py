93ee9835321a1c373c5bca9ef3ed152486ad26c2
Collin <collinoeight@aol.com>
2016-03-22 14:56:40 -0400

    First Commit

---
+++ dice.py
@@ -0,0 +1,7 @@
+from random import randint
+dice_number = input("Number of dice to be rolled: ")
+results = []
+for i in range(0, dice_number):
+    x = randint(1,6)
+    results.append(x)
+print results
