d553de8c255f3be41d368d8ddc4873935979c602
CollinRoess <collinoeight@aol.com>
2016-08-28 19:33:44 -0400

    Add files via upload

--- BFC.py
+++ BFC.py
@@ -1,55 +1,29 @@
-#!/usr/bin/env
+#!/usr/bin/python3
 
-"""The following is commented out simply because
-I'm not sure I want to keep it yet"""
+print ("Welcome to the Brainf*ck Interpreter 1.0!")
+filename=input("Please enter a filename.txt that you want to execute: ")
 
-'''print "Welcome to the Brainf*ck Interpreter 1.0!"
-filename=raw_input("Please enter a filename.txt that you want to execute: ")
+
+
 file = open(filename, 'r')
 contents=file.read()
+content_list=list(contents)
+print (content_list)
 
 pointer=[0]*30000
 index=0
-for char in contents:
+
+for char in content_list:
 	if char == "+":
 		pointer[index]+=1
 	elif char == "-":
-		pointer[index]-+1'''
+		pointer[index]-+1
+	elif char == ".":
+		print (chr(int(char)))
+	elif char == ",":
+		pointer[index]=ord(char)
 		
 		
-class Pointer:
-	def __init__(self, index, value):
-		self.index=index
-		self.value=value
+		
 
-	def add(self):
-		self.value +=1
-	
-	def sub(self):
-		self.value -=1
-		
-	def inc(self):
-		self.index +=1
-	
-	def dec(self):
-		self.index -=1
-	
-	def out(self):
-		print(self.value)
-		
-	def inp(self):
-		self.value=value
-		
-		
-"""Test of Pointer class methods
-head=Pointer(0,0)
-head.add()
-print head.value
-head.sub()
-print head.value
-head.inc()
-print head.index
-head.dec()
-print head.index
-head.out()
-"""
+print( "done")
