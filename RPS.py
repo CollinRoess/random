ab1a9b987e2f6ba922510f1f4c9da59f144698e6
CollinRoess <collinoeight@aol.com>
2016-09-01 09:42:39 -0400

    Add files via upload
    A simple rock, paper, scissors console game.

---
+++ RPS.py
@@ -0,0 +1,64 @@
+#!usr/bin/env python
+
+import time, random
+
+print("Rock, Paper, Scissors, Shoot!")
+
+def evaluate(user_choice, computer_choice):
+	possible_choices=("rock", "paper", "scissors")
+	usr_choice_index=possible_choices.index(user_choice)
+	cpu_choice_index=possible_choices.index(computer_choice)
+	
+	if usr_choice_index==0:
+		if cpu_choice_index==0:
+			time.sleep(.3)
+			print("It's a draw!")
+		if cpu_choice_index==1:
+			time.sleep(.3)
+			print("You lose!")
+		if cpu_choice_index==2:
+			time.sleep(.3)
+			print("You win!")
+		
+	if usr_choice_index==1:
+		if cpu_choice_index==0:
+			time.sleep(.3)
+			print("You win!")
+		if cpu_choice_index==1:
+			time.sleep(.3)
+			print("It's a draw!")
+		if cpu_choice_index==2:
+			time.sleep(.3)
+			print("You lose!")
+		
+	if usr_choice_index==2:
+		if cpu_choice_index==0:
+			time.sleep(.3)
+			print("You lose!")
+		if cpu_choice_index==1:
+			time.sleep(.3)
+			print("You win!")
+		if cpu_choice_index==2:
+			time.sleep(.3)
+			print("It's a draw!")
+	
+
+exit_cond= False
+while exit_cond==False:
+	time.sleep(.3)
+	user_choice=input("Enter rock, paper, or scissors: ")
+	time.sleep(.3)
+	print("You chose "+ user_choice)
+	choices=("rock", "paper", "scissors")
+	computer_choice=random.choice(choices)
+	time.sleep(.3)
+	print("Computer chose "+ computer_choice)
+	
+	evaluate(user_choice, computer_choice)
+	time.sleep(.3)
+	replay=input("Play again? y/n: ")
+	if replay=="N" or replay=="n":
+		exit_cond= True
+	elif replay=="Y" or replay=="y":
+		exit_cond= False
+	
